d6327ff5c2dbbb337867c2e220814f72961879dc
Sergey <kravchenko.me@gmail.com>
2017-12-14 21:30:34 +0300

    удалил закомментированный код

--- js/map.js
+++ js/map.js
@@ -39,18 +39,6 @@
       coordX = window.util.bounded(mapPinMain.offsetLeft - shift.x, 0, map.clientWidth);
       coordY = window.util.bounded(mapPinMain.offsetTop - shift.y, dragAreaY.min, dragAreaY.max);
 
-      // if (coordY < dragAreaY.min) {
-      //   coordY = dragAreaY.min;
-      // } else if (coordY > dragAreaY.max) {
-      //   coordY = dragAreaY.max;
-      // }
-      //
-      // if (coordX < 0) {
-      //   coordX = 0;
-      // } else if (coordX > map.clientWidth) {
-      //   coordX = map.clientWidth;
-      // }
-
       mapPinMain.style.left = coordX + 'px';
       mapPinMain.style.top = coordY + 'px';
 
